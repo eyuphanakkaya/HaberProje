4d1bee994fa19ab51dd90d36d3b051b2deaf3bf7
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2023-03-20 09:06:33 +0300

    Initial Commit

---
+++ HaberProje/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaberProje
+//
+//  Created by Eyüphan Akkaya on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
